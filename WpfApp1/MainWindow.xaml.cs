71b9cd61feffe1365ab1cc280a5d23a47d9f0985
Jake Smith <jakehockey10@gmail.com>
2017-10-05 10:43:47 -0600

    working video stream

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Accord.Video;
+using Accord.Video.DirectShow;
 
 namespace WpfApp1
 {
@@ -20,9 +11,89 @@ namespace WpfApp1
   /// </summary>
   public partial class MainWindow : Window
   {
+    private FilterInfoCollection m_videoDevices;
+    private VideoCaptureDevice m_videoDevice;
+    private VideoCapabilities[] m_videoCapabilities;
+    private VideoCapabilities[] m_snapshotCapabilities;
+
     public MainWindow()
     {
       InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      var maxVideoWidth = 0;
+      var maxSnapWidth = 0;
+      VideoCapabilities bestVideoCapabilities = null;
+      VideoCapabilities secondBestVideoCapabilities = null;
+      VideoCapabilities bestSnapshotCapabilities = null;
+      FilterInfo bestDevice = null;
+      VideoCaptureDevice bestVideoCaptureDevice = null;
+
+      // Find best device based on best snapshot resolution.
+      foreach (FilterInfo device in new FilterInfoCollection(FilterCategory.VideoInputDevice))
+      {
+        var videoCaptureDevice = new VideoCaptureDevice(device.MonikerString);
+        foreach (var capability in videoCaptureDevice.SnapshotCapabilities)
+        {
+          // Just base this on width for now.
+          if (maxSnapWidth >= capability.FrameSize.Width) continue;
+          bestVideoCaptureDevice = videoCaptureDevice;
+          maxSnapWidth = capability.FrameSize.Width;
+          bestSnapshotCapabilities = capability;
+          bestDevice = device;
+        }
+      }
+
+      if (bestDevice == null) return;
+
+      foreach (var capability in bestVideoCaptureDevice.VideoCapabilities)
+      {
+        // Just base this on width for now.
+        if (maxVideoWidth >= capability.FrameSize.Width) continue;
+        maxVideoWidth = capability.FrameSize.Width;
+        secondBestVideoCapabilities = bestVideoCapabilities;
+        bestVideoCapabilities = capability;
+      }
+      
+      m_videoDevice = bestVideoCaptureDevice;
+      // TODO: Rear-facing camera on Panasonic FZ-G1 won't work if video/snapshot resolutions are the same.  Why?
+      // https://github.com/accord-net/framework/issues/881
+      m_videoDevice.VideoResolution = secondBestVideoCapabilities;
+      m_videoDevice.ProvideSnapshots = true;
+      m_videoDevice.SnapshotResolution = bestSnapshotCapabilities;
+      m_videoDevice.SnapshotFrame += VideoDeviceOnSnapshotFrame;
+      
+      videoSourcePlayer.VideoSource = m_videoDevice;
+      videoSourcePlayer.Start();
+    }
+
+    private void VideoDeviceOnSnapshotFrame(object sender, NewFrameEventArgs eventArgs)
+    {
+      ShowSnapshot((Bitmap) eventArgs.Frame.Clone());
+    }
+
+    private void ShowSnapshot(Bitmap snapshot)
+    {
+      using (var ms = new MemoryStream())
+      {
+        snapshot.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+        //snapshot.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+      }
+      
+      //Dispatcher.Invoke(() =>
+      //{
+      //  // TODO: Show picture?
+      //});
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      if (m_videoDevice != null && m_videoDevice.ProvideSnapshots)
+      {
+        m_videoDevice.SimulateTrigger();
+      }
     }
   }
 }
